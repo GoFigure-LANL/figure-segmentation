4894cb9dd350ae4dae2dbbb1bd0a87bdf90bc601
MD RESHAD UL HOQUE <33097258+reshadshuvo123@users.noreply.github.com>
2021-12-18 17:38:30 -0500

    Add files via upload

---
+++ figure-segmentation-codes/U-Net-Segmentaion.py
@@ -0,0 +1,228 @@
+import os
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from tqdm import tqdm_notebook, tnrange
+from itertools import chain
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+
+from keras.models import Model, load_model
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
+from keras.layers.merge import concatenate, add
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+# Set some parameters
+im_width = 128
+im_height = 128
+border = 5
+path_train = r'D:\job\LAL\data\Figures_Only-20210704T024044Z-001\deep learning\training/'
+#path_train = r'D:\job\LAL\data\data\400_figures\deep-learning/'
+
+# Get and resize train images and masks
+def get_data(path, train=True):
+    ids = next(os.walk(path + "images"))[2]
+    X = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
+    if train:
+        y = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
+    print('Getting and resizing images ... ')
+    for n, id_ in tqdm_notebook(enumerate(ids), total=len(ids)):
+        # Load images
+        img = load_img(path + '/images/' + id_, grayscale=True)
+        x_img = img_to_array(img)
+        x_img = resize(x_img, (128, 128, 1), mode='constant', preserve_range=True)
+
+        # Load masks
+        if train:
+            mask = img_to_array(load_img(path + '/masks/' + id_, grayscale=True))
+            mask = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
+
+        # Save images
+        X[n, ..., 0] = x_img.squeeze() / 255
+        if train:
+            y[n] = mask / 255
+    print('Done!')
+    if train:
+        return X, y
+    else:
+        return X
+
+
+X_train, y_train = get_data(path_train, train=True)
+
+# Check if training data looks all right
+ix = random.randint(0, len(X_train))
+has_mask = y_train[ix].max() > 0
+
+fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+
+ax[0].imshow(X_train[ix, ..., 0], cmap='seismic', interpolation='bilinear')
+if has_mask:
+    ax[0].contour(y_train[ix].squeeze(), colors='k', levels=[0.5])
+ax[0].set_title('Input Images (dummy color)')
+
+ax[1].imshow(y_train[ix].squeeze(), interpolation='bilinear', cmap='gray')
+ax[1].set_title('Ground-truth');
+
+
+################## Unet #########################3
+
+def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
+    # first layer
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
+               padding="same")(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    # second layer
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
+               padding="same")(x)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    return x
+
+
+def get_unet(input_img, n_filters=16, dropout=0.5, batchnorm=True):
+    # contracting path
+    c1 = conv2d_block(input_img, n_filters=n_filters * 1, kernel_size=3, batchnorm=batchnorm)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = Dropout(dropout * 0.5)(p1)
+
+    c2 = conv2d_block(p1, n_filters=n_filters * 2, kernel_size=3, batchnorm=batchnorm)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = Dropout(dropout)(p2)
+
+    c3 = conv2d_block(p2, n_filters=n_filters * 4, kernel_size=3, batchnorm=batchnorm)
+    p3 = MaxPooling2D((2, 2))(c3)
+    p3 = Dropout(dropout)(p3)
+
+    c4 = conv2d_block(p3, n_filters=n_filters * 8, kernel_size=3, batchnorm=batchnorm)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+    p4 = Dropout(dropout)(p4)
+
+    c5 = conv2d_block(p4, n_filters=n_filters * 16, kernel_size=3, batchnorm=batchnorm)
+
+    # expansive path
+    u6 = Conv2DTranspose(n_filters * 8, (3, 3), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    u6 = Dropout(dropout)(u6)
+    c6 = conv2d_block(u6, n_filters=n_filters * 8, kernel_size=3, batchnorm=batchnorm)
+
+    u7 = Conv2DTranspose(n_filters * 4, (3, 3), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    u7 = Dropout(dropout)(u7)
+    c7 = conv2d_block(u7, n_filters=n_filters * 4, kernel_size=3, batchnorm=batchnorm)
+
+    u8 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = conv2d_block(u8, n_filters=n_filters * 2, kernel_size=3, batchnorm=batchnorm)
+
+    u9 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    u9 = Dropout(dropout)(u9)
+    c9 = conv2d_block(u9, n_filters=n_filters * 1, kernel_size=3, batchnorm=batchnorm)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    return model
+
+input_img = Input((im_height, im_width, 1), name='img')
+model = get_unet(input_img, n_filters=16, dropout=0., batchnorm=True)
+
+model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
+
+data_gen_args = dict(horizontal_flip=True,
+                     vertical_flip=True)
+image_datagen = ImageDataGenerator(**data_gen_args)
+mask_datagen = ImageDataGenerator(**data_gen_args)
+
+seed = 2018
+bs = 32
+
+image_generator = image_datagen.flow(X_train, seed=seed, batch_size=bs, shuffle=True)
+mask_generator = mask_datagen.flow(y_train, seed=seed, batch_size=bs, shuffle=True)
+
+# Just zip the two generators to get a generator that provides augmented images and masks at the same time
+train_generator = zip(image_generator, mask_generator)
+
+callbacks = [
+    EarlyStopping(patience=10, verbose=1),
+    ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.000001, verbose=1),
+    ModelCheckpoint('model-unet.h5', verbose=1, save_best_only=True, save_weights_only=True)
+]
+
+results = model.fit_generator(train_generator, steps_per_epoch=(len(X_train) // bs), epochs=100, callbacks=callbacks,
+                              validation_data=(X_train, y_train))
+
+model.save("model-unet-new1-without-tagg.h5")
+
+path_test = r'D:\job\LAL\data\data-training\test/'
+X_valid, y_valid = get_data(path_test, train=True)
+
+# Evaluate on validation set (this must be equals to the best log_loss)
+model.evaluate(X_valid, y_valid, verbose=1)
+
+# Predict on train, val and test
+preds_train = model.predict(X_train, verbose=1)
+preds_val = model.predict(X_valid, verbose=1)
+
+# Threshold predictions
+#preds_train_t = (preds_train > 0.5).astype(np.uint8)
+preds_val_t = (preds_val > 0.5).astype(np.uint8)
+
+
+def plot_sample(X, y, preds, binary_preds, ix=None):
+    if ix is None:
+        ix = random.randint(0, len(X))
+
+    has_mask = y[ix].max() > 0
+
+    fig, ax = plt.subplots(1, 4, figsize=(20, 10))
+    ax[0].imshow(X[ix, ..., 0], cmap='seismic')
+    if has_mask:
+        ax[0].contour(y[ix].squeeze(), colors='k', levels=[0.5])
+    ax[0].set_title('Original-Dummy-colors')
+
+    ax[1].imshow(y[ix].squeeze())
+    ax[1].set_title('Ground-truth Mask')
+
+    ax[2].imshow(preds[ix].squeeze(), vmin=0, vmax=1)
+    if has_mask:
+        ax[2].contour(y[ix].squeeze(), colors='k', levels=[0.5])
+    ax[2].set_title('Mask Predicted')
+
+    ax[3].imshow(binary_preds[ix].squeeze(), vmin=0, vmax=1)
+    if has_mask:
+        ax[3].contour(y[ix].squeeze(), colors='k', levels=[0.5])
+    ax[3].set_title('Predicted binary');
+
+def plot_sample1(X, y, binary_preds, ix=None):
+    if ix is None:
+        ix = random.randint(0, len(X))
+
+    has_mask = y[ix].max() > 0
+
+    fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+    ax[0].imshow(y[ix].squeeze())
+    ax[0].set_title('Ground-truth Mask')
+    ax[1].imshow(binary_preds[ix].squeeze(), vmin=0, vmax=1)
+    if has_mask:
+        ax[1].contour(y[ix].squeeze(), colors='k', levels=[0.5])
+    ax[1].set_title('Predicted binary');
+
+plot_sample(X_valid, y_valid, preds_val, preds_val_t, ix=2)
+plot_sample1(X_valid, y_valid, preds_val_t, ix=2)
+
