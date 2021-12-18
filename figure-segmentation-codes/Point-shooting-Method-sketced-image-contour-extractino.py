f8947fad9c8986076a2f58173131e86d8398384b
MD RESHAD UL HOQUE <33097258+reshadshuvo123@users.noreply.github.com>
2021-12-18 17:40:58 -0500

    Update Point-shooting-Method-sketced-image-contour-extractino.py

--- figure-segmentation-codes/Point-shooting-Method-sketced-image-contour-extractino.py
+++ figure-segmentation-codes/Point-shooting-Method-sketced-image-contour-extractino.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun July 20 22:19:53 2020
+©2020. Triad National Security, LLC. All rights reserved. This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S. Department of Energy/National Nuclear Security Administration. All rights in the program are reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear Security Administration. The Government is granted for itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare derivative works, distribute copies to the public, perform publicly and display publicly, and to permit others to do so.
+@author: Reshad
+"""
 import cv2
 import pytesseract
 import matplotlib.pyplot as plt
